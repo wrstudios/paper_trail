ce88c5e15492202fcc98033db99978a7be576b06
Jon Druse <jon@jondruse.com>
2013-07-23 09:17:13 -0700

    don't auto include; allow the user to include where it makes sense

--- lib/paper_trail.rb
+++ lib/paper_trail.rb
@@ -98,12 +98,3 @@ module PaperTrail
   end
 
 end
-
-
-ActiveSupport.on_load(:active_record) do
-  include PaperTrail::Model
-end
-
-ActiveSupport.on_load(:action_controller) do
-  include PaperTrail::Controller
-end
